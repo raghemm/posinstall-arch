d0f8b52c0f62232b71eca9be8d1123b7fa7a3a72
raghemm <edson.com@gmail.com>
2024-06-18 16:19:39 -0300

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "#Instalação de pacotes"
-pacman -S plasma-wayland-session dolphin dolphin-plugins gvfs gvfs-smb kfind konsole spectacle okular gwenview kate print-manager cups system-config-printer \
+pacman -S plasma dolphin dolphin-plugins gvfs gvfs-smb kfind konsole spectacle okular gwenview kate print-manager cups system-config-printer \
 virtualbox virtualbox-host-modules-arch power-profiles-daemon kcalc krita filelight ksystemlog kgpg partitionmanager skanlite kmousetool kcharselect krdc kompare sweeper acpid hplip usbutils \
 kamoso kdf kcachegrind krfb kbackup kwallet5 kwalletmanager kdeconnect firefox firefox-i18n-pt-br gst-plugin-va gst-plugins-bad docker docker-compose vlc qbittorrent  \
 unrar unzip p7zip mesa ark  lm_sensors i2c-tools libvdpau-va-gl libva-vdpau-driver libva-utils vdpauinfo mesa-utils ntfs-3g dosfstools exfat-utils btrfs-progs tailscale  git wget curl \
